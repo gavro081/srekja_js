57634d328cbecf6f0e67f84aae57f357477b83d1
Filip <filipgavrilovski28@gmail.com>
2025-02-16 18:00:09 +0100

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import '/stylesheets/general.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>zdr</p>
+    </>
+  )
+}
+
+export default App
+
+
+// import Router from './Router'
+// import { BrowserRouter, Route } from 'react-router-dom'
+
+// function App() {
+
+//   return (
+//     <>
+//     <BrowserRouter>
+//       <Router />
+//     </BrowserRouter>
+//     </>
+//   )
+// }
+
+// export default App
