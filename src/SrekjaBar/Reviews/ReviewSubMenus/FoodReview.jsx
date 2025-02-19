44fbd0d456d133c7c9815c0d54b740039c090eac
DavorAngelov <ddavor.angelov@gmail.com>
2025-02-19 19:06:47 +0100

    Added Circular Gallery

--- src/SrekjaBar/Reviews/ReviewSubMenus/FoodReview.jsx
+++ src/SrekjaBar/Reviews/ReviewSubMenus/FoodReview.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import styled from "styled-components";
 import GradientText from "../GradientText.jsx";
+import CircularGallery from "../CircularGallery.jsx";
+import menu_items from '../../../../public/menuitems.jsx';
 
 const FoodReview = () => {
+    const customItems = menu_items.map(item => ({
+        image: 'https://example.com/image1.jpg',
+        text: item.name
+    }));
     return (
         <Wrapper>
-
+            <div style={{ height: '500px', position: 'relative' }}>
+                <CircularGallery bend={0} textColor="#ffffff" borderRadius={0.05} items={customItems}/>
+            </div>
         </Wrapper>
     )
 }
