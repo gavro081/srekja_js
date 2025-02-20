1c99783bdcb21bc6848c9d7cb985f0b442c86e98
DavorAngelov <ddavor.angelov@gmail.com>
2025-02-20 21:36:19 +0100

    added animation for text

--- src/SrekjaBar/MenuPromotion.jsx
+++ src/SrekjaBar/MenuPromotion.jsx
@@ -11,8 +11,12 @@ import GradientText from "./Reviews/GradientText.jsx";
 import menu from "../../public/iconsSrekjaBar/restaurant-line.png"
 import reserve from "../../public/iconsSrekjaBar/add-circle-line (1).png"
 import review from "../../public/iconsSrekjaBar/chat-1-line.png"
+import SplitText from "./SplitText.jsx";
 
 export default function MenuPromotion() {
+    const handleAnimationComplete = () => {
+        console.log('All letters have animated!');
+    };
 
 
     return (
@@ -32,6 +36,17 @@ export default function MenuPromotion() {
                 </Hero>
                 <HeroImg src={cardImage}/>
             </div>
+            <h2><SplitText
+                text="Популарни понуди"
+                className="text-2xl font-semibold text-center"
+                delay={100}
+                animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
+                animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+                easing="easeOutCubic"
+                threshold={0.2}
+                rootMargin="-50px"
+                onLetterAnimationComplete={handleAnimationComplete}
+            /></h2>
             <Swiper
                 style={{maxWidth: '80%', padding: 0}}
                 modules={[Navigation]}
@@ -76,6 +91,12 @@ const Wrapper = styled.div`
 
     .mySwiper .swiper-button-next, .mySwiper .swiper-button-prev {
         color: var(--logo-green);
+    }
+    & > h2{
+        text-align: center;
+        margin: 2rem 0;
+        font-size: 3rem; 
+        
     }
 `
 
