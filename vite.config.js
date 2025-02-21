c7230837eff187290c206b9b8479f035d8ddc78d
Dimitar Arsov <dimitararsov04@gmail.com>
2025-02-21 14:56:17 +0100

    Shopping cart

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3001, // Change the port to 3001 or any other available port
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/, // Only include files in the src directory
+  },
+});
