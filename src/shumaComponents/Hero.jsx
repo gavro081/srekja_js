197b25f10199e6e65ff661432a1738b61230bad7
Andrej <asumanovski@yahoo.com>
2025-02-22 13:56:38 +0100

    Napraven hero section poubav

--- src/shumaComponents/Hero.jsx
+++ src/shumaComponents/Hero.jsx
@@ -5,8 +5,30 @@ export default function Hero() {
   return (
     <HeroContainer>
       <HeroContent>
-        <LeftContainer>Среќа е...</LeftContainer>
-        <RightContainer />
+        <AnimationContainer>
+          <LeftContainer>Среќа е...</LeftContainer>
+          <RightContainer />
+        </AnimationContainer>
+
+        <CallToAction>
+          <Paragraph>
+            {" "}
+            Среќа.мк, платформата која ги спојува инвеститорите со стартапите,
+            фирмите со фриленсерите и читателите со среќата
+          </Paragraph>
+          <div
+            style={{
+              display: "flex",
+              gap: "20px",
+              marginTop: "10px",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <NextButton>Стартап Настани</NextButton>
+            <NextButton>Вработи Сé</NextButton>
+          </div>
+        </CallToAction>
       </HeroContent>
     </HeroContainer>
   );
@@ -16,17 +38,23 @@ const HeroContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 45vh;
+  height: 75vh;
+  background-image: url("../../assets/images/heroBackGround.jpg");
+  background-size: cover;
+  background-position: center;
 `;
 
 const HeroContent = styled.div`
   width: 70%;
-  height: 50%;
+  height: 60%;
   display: flex;
   justify-content: center;
+  flex-direction: column;
+
   align-items: center;
   box-shadow: 1px 1px 30px rgba(0, 0, 0, 0.5);
   padding: 0 20px;
+  background-color: rgba(255, 255, 255, 0.5);
 `;
 
 const LeftContainer = styled.div`
@@ -39,4 +67,56 @@ const LeftContainer = styled.div`
   font-family: "Lobster", cursive;
   font-size: 80px;
   margin-right: 20px;
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  border: none;
+  color: white;
+  font-size: 20px;
+  cursor: pointer;
+  border-radius: 10px;
+  background: linear-gradient(to right, #0b776f, #e96f23, #e9ba1d, #d41e47);
+`;
+
+const AnimationContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const CallToAction = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-family: "Lobster", cursive;
+  font-size: 80px;
+  margin-top: 40px;
+`;
+
+const Paragraph = styled.p`
+  font-size: 16px;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+const NextButton = styled.button`
+  background: linear-gradient(45deg, var(--logo-orange), var(--logo-red));
+  color: white;
+  border: none;
+  padding: 0.7rem 2rem;
+  font-size: 1.4rem;
+  letter-spacing: 1px;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: 0.3s ease-in-out;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
+
+  &:hover {
+    background: linear-gradient(45deg, var(--logo-red), var(--logo-orange));
+    transform: scale(1.05);
+  }
 `;
